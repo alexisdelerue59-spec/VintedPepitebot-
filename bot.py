ad1d00d4ad8b1769d8a9e2c7e43ce26785348b70
alexisdelerue59-spec <alexisdelerue59@gmail.com>
2026-09-11 13:58:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"].strip()
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"].replace("\n", "").replace("\r", "").strip()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
